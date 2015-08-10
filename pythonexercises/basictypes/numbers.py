4ad9514e668cf2e1bde0f7ef27e410276736e737
KarenKawaii <karenamaroc@gmail.com>
2015-08-09 22:33:23 -0500

    [ADD] pythonexercises: Adding numbers.py

---
+++ pythonexercises/basictypes/numbers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+This module represents the type numbers:
+Reals
+With exponent
+Compound
+"""
+
+# Representation of real numbers.
+REAL = 0.2703
+# Representation with exponent.
+REAL2 = 0.1e-3
+#Compound numbers.
+COMPOUND = 2.1 + 7.8j
+
+# Print the numbers type
+print type(REAL)
+print type(REAL2)
+print type(COMPOUND)
