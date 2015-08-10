cc29e599000be486f355ffff747909ef862a0f28
KarenKawaii <karenamaroc@gmail.com>
2015-08-09 22:27:16 -0500

    [ADD] pythonexercises: Adding types.py

---
+++ pythonexercises/basictypes/types.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+This module verify the var type.
+"""
+
+# This is a string.
+C = "Hello World"
+# This is an integer.
+E = 23
+
+# The type function proves the var type.
+print type(C)
+print type(E)
